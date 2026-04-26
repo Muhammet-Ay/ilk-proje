83f16f96e2acd82157ab1efaefb0d672048a285b
Muhammet-Ay <aym389468@gmail.com>
2026-04-26 16:42:33 +0300

    feat: ilk OOP denemesi eklendi

---
+++ main.py
@@ -0,0 +1,12 @@
+class User:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def introduce(self):
+        print(f"Merhaba, ben {self.name}! {self.age} yaşındayım.")
+
+
+user1 = User("Muhammet", 20)
+user1.introduce()
+        
